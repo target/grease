bbd2d5f7212d387e0f6016d154f66fd0a51fa1e1
James E. Bell Jr <github@travelerbell.net>
2017-09-29 21:04:00 -0500

    More fixes for test runs
    Signed-off-by: James E. Bell Jr <github@travelerbell.net>

---
+++ tgt_grease_core_util/tests/__init__.py
@@ -0,0 +1,3 @@
+from .test_greaseUtility import test_greaseUtility
+from .test_importTools import test_importTools
+from .test_logging import test_logging
---
+++ tgt_grease_enterprise/Detectors/tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_time_delta import TestTimeDelta

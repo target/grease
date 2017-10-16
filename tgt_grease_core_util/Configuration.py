789484b5dd018e5c93882a300545c0f3628ed383
James.E.Bell <james.e.bell@target.com>
2017-10-16 06:26:15 -0500

    Fixed dir endings
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease_core_util/Configuration.py
+++ tgt_grease_core_util/Configuration.py
@@ -11,14 +11,14 @@ class Configuration(object):
 
     _config = {}
     if os.name == 'nt':
-        grease_dir = "C:\\grease\\"
+        grease_dir = "C:\\grease"
     else:
-        grease_dir = "/var/tmp/grease" + os.sep
+        grease_dir = "/var/tmp/grease"
     fs_Separator = os.sep
     op_name = os.name
     grease_log = grease_dir + os.sep + "grease.log"
     identity_file = grease_dir + os.sep + "grease_identity.txt"
-    mock_dir = grease_dir + os.sep + "mock" + os.sep
+    opt_dir = grease_dir + os.sep + "opt" + os.sep
     identity = None
     _node_db_id = None
 
@@ -26,8 +26,8 @@ class Configuration(object):
         # Ensure the GREASE Dir
         if not os.path.isdir(self.grease_dir):
             os.mkdir(self.grease_dir)
-        if not os.path.isdir(self.mock_dir):
-            os.mkdir(self.mock_dir)
+        if not os.path.isdir(self.opt_dir):
+            os.mkdir(self.opt_dir)
         # load up config
         self._load_config()
 

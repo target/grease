222426d7cbbb89ba1c052e7c6f868a1429e0cf29
James.E.Bell <james.e.bell@target.com>
2017-10-16 06:42:51 -0500

    Added Mock Data Support
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease_enterprise/BaseSourceClass.py
+++ tgt_grease_enterprise/BaseSourceClass.py
@@ -1,8 +1,30 @@
 from abc import ABCMeta, abstractmethod
+import json
+import os
+import fnmatch
+from tgt_grease_core_util import Configuration
 
 
 class BaseSource(object):
     __metaclass__ = ABCMeta
+
+    def mock_data(self):
+        # type: () -> list
+        conf = Configuration()
+        matches = []
+        final = []
+        for root, dirnames, filenames in os.walk(conf.opt_dir):
+            for filename in fnmatch.filter(filenames, '*.mock.{0}.json'.format(self.__class__.__name__)):
+                matches.append(os.path.join(root, filename))
+        for doc in matches:
+            with open(doc) as current_file:
+                content = current_file.read().replace('\r\n', '')
+            try:
+                for res in json.loads(content):
+                    final.append(res)
+            except ValueError:
+                continue
+        return final
 
     @abstractmethod
     def parse_source(self, rule_config):

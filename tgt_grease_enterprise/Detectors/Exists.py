4b46b470c8e5e379435eb2e8861e4250d7f7ad51
Cory.Hollinger <Cory.Hollinger@target.com>
2017-10-25 09:47:22 -0500

    added comments

--- tgt_grease_enterprise/Detectors/Exists.py
+++ tgt_grease_enterprise/Detectors/Exists.py
@@ -1,6 +1,19 @@
 from .BaseDetectorClass import BaseDetector
 
 
+# Basic Exists Detector Config
+# ...
+# "exists": [
+#    {
+#       "field": "my_field", <- Mandatory
+#       "variable": True, <- Optional Assignment Parameter
+#       "variable_name": "my_field" <- Optional Assignment Parameter
+#    }
+# ]
+#
+# ...
+
+
 class Exists(BaseDetector):
     def __init__(self):
         self._result = {'result': False}
@@ -10,13 +23,16 @@ class Exists(BaseDetector):
         rules_needed_to_pass = len(rules)
 
         for rule in rules:
+            # Malformed rule; need 'field'
             if 'field' not in rule:
                 self._result['result'] = False
                 return
 
+            # If the source contains the field, we passed
             if rule['field'] in source:
                 rules_passed += 1
 
+                # If we want to capture the field as a variable, add it to the final result
                 if 'variable' in rule:
                     if rule['variable'] and 'variable_name' in rule:
                         self._result[rule['variable_name']] = source[rule['field']]

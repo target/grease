a974619131dbf8e26b2a1472f5b327519ca9c3d4
gamagori <grant.mattke.gordon@gmail.com>
2017-11-01 18:40:24 -0500

    added a more reasonable URLSource, pending discussion on formatting

--- tgt_grease_enterprise/Sources/URLSource.py
+++ tgt_grease_enterprise/Sources/URLSource.py
@@ -1,23 +1,23 @@
 import os
 import json
+import requests
+import time
 from .BaseSourceClass import BaseSource
 from tgt_grease_core_util import Configuration
 
 
 class URLSource(BaseSource):
     def __init__(self):
-        print('newing up URLSource')
         self._result = {}
 
-    def _get_online(self, host):
-        if os.name == 'nt':
-            parameters = "-n 1"
-        else:
-            parameters = "-q -c 1"
+    def _dress_url(self, url):
+        return 'http://' + url
 
-        call = "ping " + parameters + " " + host
-        print call
-        return os.system(call)
+    def _get_request(self, url):
+        try:
+            return requests.get(url)
+        except:
+            return -1
 
     def mock_data(self):
         #inherited
@@ -25,12 +25,17 @@ class URLSource(BaseSource):
 
     def parse_source(self, rule_config):
         # type (dict) -> None
-        strip_header = json.loads(rule_config)['URLSource'][0]
-
-        for url in strip_header['url']:
-            continue
-            #self._result.append(self._get_online(url))
-
+        for entry in rule_config['URLSource']:
+            for url in entry['url']:
+                dressed = self._dress_url(url)
+                response = (self._get_request(dressed))
+                self._result[url] = {}
+                self._result[url]['url'] = response.url
+                #self._result[url]['text'] = response.text
+                self._result[url]['elapsed'] = response.elapsed
+                self._result[url]['encoding'] = response.encoding
+                self._result[url]['headers'] = response.headers
+                self._result[url]['status_code'] = response.status_code
 
     def get_records(self):
         # type: () -> dict

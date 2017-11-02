86c62f57895a5fa0be0b6928796f6af99dd8f50c
gamagori <grant.mattke.gordon@gmail.com>
2017-11-02 11:12:58 -0500

    cast everything to str for dedupe engine. make sure function call is only overwritten once.

--- tgt_grease_enterprise/Sources/URLSource.py
+++ tgt_grease_enterprise/Sources/URLSource.py
@@ -1,30 +1,32 @@
 import os
 import json
 import requests
-import time
 from .BaseSourceClass import BaseSource
 from tgt_grease_core_util import Configuration
 
+# Overwrite getresponse, so it saves host ip/port
+import httplib
+def getresponse(self, *args, **kwargs):
+    response = self._old_getresponse(*args, **kwargs)
+    if self.sock:
+        response.peer = self.sock.getpeername()
+    else:
+        response.peer = None
+    return response
+
+httplib.HTTPConnection._old_getresponse =\
+        httplib.HTTPConnection.getresponse
+httplib.HTTPConnection.getresponse = getresponse
 
 class URLSource(BaseSource):
     def __init__(self):
         self._result = []
-
-        # Overwrite getresponse, so it saves host ip/port
-        import httplib
-        def getresponse(self, *args, **kwargs):
-            response = self._old_getresponse(*args, **kwargs)
-            if self.sock:
-                response.peer = self.sock.getpeername()
-            else:
-                response.peer = None
-            return response
-
-        httplib.HTTPConnection._old_getresponse =\
-                httplib.HTTPConnection.getresponse
-        httplib.HTTPConnection.getresponse = getresponse
-
+           
     def _dress_url(self, url):
+        if 'http://' in url:
+            return url
+        if 'https://' in url:
+            return url
         return 'http://' + url
 
     def _get_request(self, url):
@@ -35,33 +37,37 @@ class URLSource(BaseSource):
             print e
             return -1
 
+    def _get_peer(self, resp):
+        #pull IP and port 
+        resp = resp.raw._original_response
+        if hasattr(resp, 'peer'):
+            setattr(resp, 'host_ip', getattr(resp, 'peer')[0])
+            setattr(resp, 'host_port', getattr(resp, 'peer')[1])
+        else:
+            setattr(resp, 'host_ip', '')
+            setattr(resp, 'host_port', '')
+        return resp
+
     def parse_source(self, rule_config):
         # type (dict) -> None
         # rule_config is an array of lists.
-        for entry in rule_config['URLSource']:
+        for entry in rule_config:
             for url in entry['url']:
                 dressed = self._dress_url(url)
                 response = (self._get_request(dressed))
-
-                #pull IP and port 
-                orig = response.raw._original_response
-                if hasattr(orig, 'peer'):
-                    response_host_ip = str(getattr(orig, 'peer')[0])
-                    response_host_port = str(getattr(orig, 'peer')[1])
-                else:
-                    response_host_ip = ''
-                    response_host_port = ''
+                peer = self._get_peer(response)
 
                 #self._result needs to be an array of 1D lists.
+                #looks like grease SourceDeDuplify encodes to JSON
                 self._result.append({
-                    'content': response.content,
-                    'content_length': len(response.content),
-                    'elapsed': response.elapsed,
-                    'headers': response.headers,
-                    'response_host_ip': response_host_ip,
-                    'response_host_port': response_host_port,
-                    'status_code': response.status_code,
-                    'url': url
+                    'content': str(response.content),
+                    'content_length': str(len(response.content)),
+                    'elapsed': str(response.elapsed),
+                    'headers': str(response.headers),
+                    'response_host_ip': str(peer.host_ip),
+                    'response_host_port': str(peer.host_port),
+                    'status_code': str(response.status_code),
+                    'url': str(url)
                 })
 
     def get_records(self):
@@ -71,5 +77,4 @@ class URLSource(BaseSource):
     def mock_data(self):
         # inherited
         pass
-
 

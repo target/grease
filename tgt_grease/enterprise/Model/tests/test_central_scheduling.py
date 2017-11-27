13e4ea9d83d72d2e40518d48b402903d2eaaa8e2
James.E.Bell <james.e.bell@target.com>
2017-11-27 11:32:07 -0600

    Added Test for schedule Scheduling
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease/enterprise/Model/tests/test_central_scheduling.py
+++ tgt_grease/enterprise/Model/tests/test_central_scheduling.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 from tgt_grease.core import GreaseContainer
-from tgt_grease.enterprise.Model import Scheduling
+from tgt_grease.enterprise.Model import Scheduling, Detect, PrototypeConfig, Scheduler, Deduplication
 from bson.objectid import ObjectId
-from datetime import datetime
+import datetime
 import platform
 import time
+import pymongo
 
 
 class TestScheduling(TestCase):
@@ -19,7 +20,7 @@ class TestScheduling(TestCase):
                 'roles': ["general"],
                 'prototypes': ["detect"],
                 'active': True,
-                'activationTime': datetime.utcnow()
+                'activationTime': datetime.datetime.utcnow()
         }).inserted_id
         time.sleep(1.5)
         self.assertFalse(sch.scheduleDetection('test', 'test_conf', []))
@@ -37,7 +38,7 @@ class TestScheduling(TestCase):
                 'roles': ["general"],
                 'prototypes': ["detect"],
                 'active': True,
-                'activationTime': datetime.utcnow()
+                'activationTime': datetime.datetime.utcnow()
         }).inserted_id
         time.sleep(1)
         jID2 = jServer.insert_one({
@@ -46,7 +47,7 @@ class TestScheduling(TestCase):
                 'roles': ["general"],
                 'prototypes': ["detect"],
                 'active': True,
-                'activationTime': datetime.utcnow()
+                'activationTime': datetime.datetime.utcnow()
         }).inserted_id
         time.sleep(1)
         self.assertTrue(sch.scheduleDetection('test', 'test_conf', [
@@ -145,3 +146,106 @@ class TestScheduling(TestCase):
         jServer.delete_one({'_id': ObjectId(jID1)})
         jServer.delete_one({'_id': ObjectId(jID2)})
         ioc.getCollection('SourceData').drop()
+
+    def test_scheduleScheduling(self):
+        d = Detect()
+        p = PrototypeConfig(d.ioc)
+        s = Scheduler(d.ioc)
+        source = {
+            'key': 'var',
+            'ver': 'key',
+            'greg': 'old',
+            'created': '2017-11-24'
+        }
+        configuration = {
+            'name': 'demoConfig',
+            'job': 'otherThing',
+            'exe_env': 'general',
+            'source': 'Google',
+            'logic': {
+                'DateRange': [
+                    {
+                        'field': 'created',
+                        'format': '%Y-%m-%d',
+                        'min': '2017-11-23',
+                        'max': '2017-11-25'
+                    }
+                ],
+                'Regex': [
+                    {
+                        'field': 'key',
+                        'pattern': '.*',
+                        'variable': True,
+                        'variable_name': 'field'
+                    },
+                    {
+                        'field': 'ver',
+                        'pattern': '.*'
+                    }
+                ],
+                'Exists': [
+                    {
+                        'field': 'greg',
+                        'variable': True,
+                        'variable_name': 'greg'
+                    }
+                ]
+            }
+        }
+        p.load(True, [configuration])
+        sourceId = d.ioc.getCollection('SourceData').insert_one({
+                    'grease_data': {
+                        'sourcing': {
+                            'server': ObjectId(d.ioc.getConfig().NodeIdentity)
+                        },
+                        'detection': {
+                            'server': ObjectId(d.ioc.getConfig().NodeIdentity),
+                            'detectionStart': None,
+                            'detectionEnd': None,
+                            'detection': {}
+                        },
+                        'scheduling': {
+                            'schedulingServer': None,
+                            'schedulingStart': None,
+                            'schedulingEnd': None
+                        },
+                        'execution': {
+                            'server': None,
+                            'assignmentTime': None,
+                            'executionStart': None,
+                            'executionEnd': None,
+                            'context': {},
+                            'executionSuccess': False,
+                            'commandSuccess': False,
+                            'failures': 0,
+                            'retryTime': datetime.datetime.utcnow()
+                        }
+                    },
+                    'source': str('test').encode('utf-8'),
+                    'configuration': configuration.get('name'),
+                    'data': source,
+                    'createTime': datetime.datetime.utcnow(),
+                    'expiry': Deduplication.generate_max_expiry_time(1)
+        }).inserted_id
+        scheduleServer = d.ioc.getCollection('JobServer').insert_one({
+                'jobs': 0,
+                'os': platform.system().lower(),
+                'roles': ["general"],
+                'prototypes': ["schedule"],
+                'active': True,
+                'activationTime': datetime.datetime.utcnow()
+        }).inserted_id
+        self.assertTrue(d.detectSource())
+        self.assertFalse(d.getScheduledSource())
+        self.assertTrue(len(
+            d.ioc.getCollection('SourceData').find_one(
+                {
+                    'grease_data.scheduling.server': ObjectId(scheduleServer),
+                    'grease_data.scheduling.start': None,
+                    'grease_data.scheduling.end': None
+                },
+                sort=[('grease_data.createTime', pymongo.DESCENDING)]
+            )
+        ))
+        d.ioc.getCollection('JobServer').delete_one({'_id': ObjectId(scheduleServer)})
+        d.ioc.getCollection('SourceData').delete_one({'_id': ObjectId(sourceId)})

2eab1e7f02a41b28c7537c097e960579264c2aba
James.E.Bell <james.e.bell@target.com>
2017-11-27 09:26:57 -0600

    Added method to get sources
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease/enterprise/Model/Scheduler.py
+++ tgt_grease/enterprise/Model/Scheduler.py
@@ -1,6 +1,8 @@
 from tgt_grease.core import GreaseContainer, ImportTool
 from .Configuration import PrototypeConfig
 from .CentralScheduling import Scheduling
+from bson.objectid import ObjectId
+import pymongo
 
 
 class Scheduler(object):
@@ -34,3 +36,19 @@ class Scheduler(object):
 
         """
         pass
+
+    def getDetectedSource(self):
+        """Gets the oldest successfully detected source
+
+        Returns:
+            dict: Object from MongoDB
+
+        """
+        return self.ioc.getCollection('SourceData').find_one(
+            {
+                    'grease_data.scheduling.server': ObjectId(self.ioc.getConfig().NodeIdentity),
+                    'grease_data.scheduling.start': None,
+                    'grease_data.scheduling.end': None
+            },
+            sort=[('grease_data.createTime', pymongo.DESCENDING)]
+        )

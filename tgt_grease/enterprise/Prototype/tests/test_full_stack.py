548af7728b111193022c67013e225090e2052c3a
James.E.Bell <james.e.bell@target.com>
2017-11-30 17:58:10 -0600

    Mock Scanning Tested
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease/enterprise/Prototype/tests/test_full_stack.py
+++ tgt_grease/enterprise/Prototype/tests/test_full_stack.py
@@ -1,7 +1,95 @@
 from unittest import TestCase
+from tgt_grease.core import GreaseContainer
+from tgt_grease.enterprise.Model import PrototypeConfig
+from tgt_grease.enterprise.Prototype import scan, detect, schedule
+from bson.objectid import ObjectId
+import json
+import os
 
 
-class TestMockedSource(TestCase):
+class TestFullStack(TestCase):
 
     def test_sourcing(self):
-        self.assertTrue(True)
+        #############################################
+        #            SETUP UP TIME
+        #############################################
+        ioc = GreaseContainer()
+        pConf = PrototypeConfig(ioc)
+        ioc.ensureRegistration()
+        ioc.getCollection('JobServer').update_one(
+            {'_id': ObjectId(ioc.getConfig().NodeIdentity)},
+            {
+                '$set': {
+                    'prototypes': ['scan', 'detect', 'schedule']
+                }
+            }
+        )
+        ioc.getCollection('Configuration').insert_one(
+            {
+                'active': True,
+                'type': 'prototype_config',
+                "name": "full_stack_test",
+                "job": "help",
+                "exe_env": "general",
+                "source": "url_source",
+                "logic": {
+                    "Regex": [
+                        {
+                            "field": "url",
+                            "pattern": ".*",
+                            'variable': True,
+                            'variable_name': 'url'
+                        }
+                    ],
+                    'Range': [
+                        {
+                            'field': 'status_code',
+                            'min': 199,
+                            'max': 201
+                        }
+                    ]
+                }
+            }
+        )
+        pConf.load(reloadConf=True)
+        TestFile = ioc.getConfig().get('Sourcing', 'dir') + 'full_stack.mock.url.json'
+        with open(TestFile, 'w') as fil:
+            fil.write(json.dumps({
+                'url': 'https://google.com',
+                'status_code': 200,
+                'headers': str({'test': 'var'}),
+                'body': 'test data'
+            }, indent=4))
+        #############################################
+        #            EXECUTE SCANNING
+        #############################################
+        Scanner = scan()
+        Scanner.ioc.getLogger().getConfig().set('verbose', True, 'Logging')
+        Scanner.ioc.getLogger().getConfig().set('trace', True, 'Logging')
+        Scanner.ioc.getLogger().getConfig().set('mock', True, 'Sourcing')
+        Scanner.ioc.getLogger().getConfig().set('config', 'full_stack_test', 'Sourcing')
+        self.assertTrue(Scanner.execute({'loop': 1}))
+        #############################################
+        #            ASSERT SCANNING
+        #############################################
+        self.assertTrue(ioc.getCollection('SourceData').find_one({
+            'grease_data.sourcing.server': ObjectId(ioc.getConfig().NodeIdentity),
+            'grease_data.detection.server': ObjectId(ioc.getConfig().NodeIdentity),
+            'grease_data.detection.start': None,
+            'grease_data.detection.end': None
+        }))
+        #############################################
+        #            CLEAN UP TIME
+        #############################################
+        pConf.load(reloadConf=True)
+        ioc.getCollection('JobServer').update_one(
+            {'_id': ObjectId(ioc.getConfig().NodeIdentity)},
+            {
+                '$set': {
+                    'prototypes': []
+                }
+            }
+        )
+        ioc.getCollection('Configuration').drop()
+        ioc.getCollection('SourceData').drop()
+        os.remove(TestFile)

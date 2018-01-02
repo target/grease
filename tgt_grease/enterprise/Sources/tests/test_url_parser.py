f919060d1939a78bacf49d525bfca0c451f4cd98
James.E.Bell <james.e.bell@target.com>
2018-01-02 16:10:58 -0600

    Adjusted fail tests for wierd UTC issues
    Signed-off-by: James.E.Bell <james.e.bell@target.com>

--- tgt_grease/enterprise/Sources/tests/test_url_parser.py
+++ tgt_grease/enterprise/Sources/tests/test_url_parser.py
@@ -117,7 +117,7 @@ class TestUrlParser(TestCase):
             'job': 'example_job',
             'exe_env': 'general',
             'source': 'url_source',
-            'hour': (datetime.datetime.utcnow() + datetime.timedelta(hours=2)).hour,
+            'hour': (datetime.datetime.utcnow() + datetime.timedelta(hours=6)).hour,
             'url': ['google.com'],
             'logic': {}
         }))
@@ -131,7 +131,7 @@ class TestUrlParser(TestCase):
             'job': 'example_job',
             'exe_env': 'general',
             'source': 'url_source',
-            'minute': (datetime.datetime.utcnow() + datetime.timedelta(minutes=2)).minute,
+            'minute': (datetime.datetime.utcnow() + datetime.timedelta(minutes=10)).minute,
             'url': ['google.com'],
             'logic': {}
         }))
@@ -145,7 +145,7 @@ class TestUrlParser(TestCase):
             'job': 'example_job',
             'exe_env': 'general',
             'source': 'url_source',
-            'hour': (datetime.datetime.utcnow() + datetime.timedelta(hours=2)).hour,
+            'hour': (datetime.datetime.utcnow() + datetime.timedelta(hours=6)).hour,
             'minute': (datetime.datetime.utcnow() + datetime.timedelta(minutes=2)).minute,
             'url': ['google.com'],
             'logic': {}

7d79c42a4b0ec988ee2991248125264c81565b40
James E. Bell Jr <james.e.bell@target.com>
2018-01-24 09:04:14 -0600

    Fixed `hWaitStop` property access
    Signed-off-by: James E. Bell Jr <james.e.bell@target.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='tgt_grease',
-    version='2.0.7',
+    version='2.0.8',
     license="MIT",
     description='GRE Application Service Engine',
     long_description="""

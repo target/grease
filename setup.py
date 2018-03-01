8f1912ede8f71a52555734964197bd65641dfbe2
Ronald Queensen <Ronald.Queensen@target.com>
2018-03-01 15:34:24 -0600

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='tgt_grease',
-    version='2.2.0',
+    version='2.2.1',
     license="MIT",
     description='GRE Application Service Engine',
     long_description="""

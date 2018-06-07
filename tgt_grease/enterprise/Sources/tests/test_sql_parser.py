eb20f621ec9c9c7dac1d917aa7f6af1f1b5f6fff
Ronald Queensen <Ronald.Queensen@target.com>
2018-06-07 14:17:16 -0500

    confirmed that fix works, made it more explicit

--- tgt_grease/enterprise/Sources/tests/test_sql_parser.py
+++ tgt_grease/enterprise/Sources/tests/test_sql_parser.py
@@ -44,6 +44,7 @@ class TestSQLSource(TestCase):
             if not os.environ.get('GREASE_TEST_DSN_ORIGINAL'):
                 os.environ['GREASE_TEST_DSN_ORIGINAL'] = os.environ.get('GREASE_TEST_DSN')
             os.environ['GREASE_TEST_DSN'] = os.environ['GREASE_TEST_DSN'] + " dbname=test_data"
+            
         except psycopg2.Error as e:
             print("psycopg2 Exception occurred, {}".format(e.pgerror))
 

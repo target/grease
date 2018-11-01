6a5d4b78f6f27f043fdd392f52ba9d487e3e614d
Cory Hollinger <cory.hollinger@target.com>
2018-11-01 15:16:26 -0500

    fixed bug

--- tgt_grease/core/Decorators/grease_log.py
+++ tgt_grease/core/Decorators/grease_log.py
@@ -6,19 +6,21 @@ from tgt_grease.core.Types import Command
 
 def grease_log(wrapped):
     def log(*args, **kwargs):
+        # Get the first instance of a command or source from the stack
         stack = inspect.stack()
+        
+        calling_class = "Unknown"
         for stack_item in stack:
             self_context = stack_item[0].f_locals.get("self")
             if isinstance(self_context, Command) or isinstance(self_context, BaseSourceClass):
                 calling_class = self_context.__class__.__name__
                 break
-            else:
-                calling_class = "Unknown"
-                break
 
-        GreaseContainer().getLogger().info(f"Starting execution of {wrapped.__name__} from class {calling_class}.")
+        ioc = GreaseContainer()
+
+        ioc.getLogger().info(f"Starting execution of {wrapped.__name__} from Command or Source {calling_class}.")
         wrapped(*args, **kwargs)
-        GreaseContainer().getLogger().info(f"Finished execution of {wrapped.__name__} from class {calling_class}.")
+        ioc.getLogger().info(f"Finished execution of {wrapped.__name__} from Command or Source {calling_class}.")
 
     return log
 

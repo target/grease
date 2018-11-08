a146f1f36541751e0bb6fd35a84e660c80e81f23
James E. Bell Jr <james.e.bell@target.com>
2018-11-08 13:01:39 -0600

    Deleted orphaned job
    Signed-off-by: James E. Bell Jr <james.e.bell@target.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='tgt_grease',
-    version='2.4.0',
+    version='2.4.1',
     license="MIT",
     description='Modern distributed automation engine built with love by Target',
     long_description="""
